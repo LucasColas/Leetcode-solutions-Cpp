872690cb29df3a402e4c5d624eaa07992054410a
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-04-14 10:25:27 +0200

    Create 700. - Search in a Binary Search Tree.cpp

---
+++ 700. - Search in a Binary Search Tree.cpp
@@ -0,0 +1,30 @@
+/*
+
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+From Leetcode : https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (!root) return NULL;
+        if (root->val == val) return root;
+        return searchBST(root->val > val ? root->left : root->right, val);
+    }
+};
