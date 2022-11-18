0959cbdf681d976014224b5523c0631884a66fd6
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-11-18 18:52:51 +0100

    Update 263 - Ugly Number.cpp

--- 263 - Ugly Number.cpp
+++ 263 - Ugly Number.cpp
@@ -1,3 +1,10 @@
+/*
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer n, return true if n is an ugly number.
+https://leetcode.com/problems/ugly-number/
+*/
+
 class Solution {
 public:
     bool isUgly(int n) {
